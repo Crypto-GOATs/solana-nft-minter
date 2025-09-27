66c25d8f824723a980d5dbfcbb22675be7530583
Tommaso Lunardon <tommaso.lunardon.97@gmail.com>
2025-09-26 22:25:20 -0400

    Added headers to avoid banner in Phantom

--- next.config.js
+++ next.config.js
@@ -5,4 +5,16 @@ module.exports = {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+        ],
+      },
+    ];
+  },
 };
