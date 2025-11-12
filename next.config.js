e7da9622ba638095c85610babfd3aff1f8cf7f70
Tommaso Lunardon <tommaso.lunardon.97@gmail.com>
2025-11-12 17:45:05 -0500

    Updated next.config.js to enable solana libraries

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.resolve.alias['@'] = path.resolve(__dirname);
+    
+    // Fix Solana Web3.js bundling issues on server
+    if (isServer) {
+      config.externals.push({
+        '@solana/web3.js': 'commonjs @solana/web3.js',
+        '@solana/spl-token': 'commonjs @solana/spl-token',
+      });
+    }
+    
+    // Ignore node-specific modules when bundling
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+    };
+    
     return config;
   },
   async headers() {
